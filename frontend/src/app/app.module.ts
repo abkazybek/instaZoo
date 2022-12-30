b5485c697fa58e6946c6c6a752b2ca1bf57effcc
Kazybek Ablay <kazbek529@gmail.com>
2022-12-31 02:54:04 +0600

    commit fronted

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MaterialModule} from './material-module';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {authInterceptorProviders} from './helper/auth-interceptor.service';
+import {authErrorInterceptorProviders} from './helper/error-interceptor.service';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { NavigationComponent } from './layout/navigation/navigation.component';
+import { IndexComponent } from './layout/index/index.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    NavigationComponent,
+    IndexComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MaterialModule,
+    HttpClientModule,
+    FormsModule
+  ],
+
+  providers: [authInterceptorProviders, authErrorInterceptorProviders],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
