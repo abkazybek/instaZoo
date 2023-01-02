59ee2c2afb06797d25dd9ce4264ebef9189b64cc
Kazybek Ablay <kazbek529@gmail.com>
2023-01-03 00:39:48 +0600

    final commit

--- backend/src/main/java/com/example/demo/web/CommentController.java
+++ backend/src/main/java/com/example/demo/web/CommentController.java
@@ -18,15 +18,15 @@ import java.security.Principal;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@CrossOrigin
 @RestController
 @RequestMapping("api/comment")
+@CrossOrigin
 public class CommentController {
 
     @Autowired
-    private CommentFacade commentFacade;
-    @Autowired
     private CommentService commentService;
+    @Autowired
+    private CommentFacade commentFacade;
     @Autowired
     private ResponseErrorValidation responseErrorValidation;
 
@@ -34,18 +34,18 @@ public class CommentController {
     public ResponseEntity<Object> createComment(@Valid @RequestBody CommentDTO commentDTO,
                                                 @PathVariable("postId") String postId,
                                                 BindingResult bindingResult,
-                                                Principal principal){
+                                                Principal principal) {
         ResponseEntity<Object> errors = responseErrorValidation.mapValidationService(bindingResult);
         if (!ObjectUtils.isEmpty(errors)) return errors;
 
-        Comment comment = commentService.saveComment(Long.parseLong(postId),commentDTO, principal);
+        Comment comment = commentService.saveComment(Long.parseLong(postId), commentDTO, principal);
         CommentDTO createdComment = commentFacade.commentToCommentDTO(comment);
 
         return new ResponseEntity<>(createdComment, HttpStatus.OK);
-
     }
-    @PostMapping("/{postId}/all")
-    public ResponseEntity<List<CommentDTO>> getAllCommentsToPost(@PathVariable("postId") String postId){
+
+    @GetMapping("/{postId}/all")
+    public ResponseEntity<List<CommentDTO>> getAllCommentsToPost(@PathVariable("postId") String postId) {
         List<CommentDTO> commentDTOList = commentService.getAllCommentsForPost(Long.parseLong(postId))
                 .stream()
                 .map(commentFacade::commentToCommentDTO)
@@ -53,10 +53,11 @@ public class CommentController {
 
         return new ResponseEntity<>(commentDTOList, HttpStatus.OK);
     }
+
     @PostMapping("/{commentId}/delete")
     public ResponseEntity<MessageResponse> deleteComment(@PathVariable("commentId") String commentId) {
         commentService.deleteComment(Long.parseLong(commentId));
         return new ResponseEntity<>(new MessageResponse("Post was deleted"), HttpStatus.OK);
-
     }
+
 }
